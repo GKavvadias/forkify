f326603490168dd3646e16416d7dd3a7ec7d366f
GKavvadias <il_gato_nero@hotmail.com>
2024-06-05 16:41:44 +0300

    'initial'

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '73f1f204-ef7d-448f-81ed-543161ec9375';
+export const MODAL_CLOSE_SEC = 2.5;
